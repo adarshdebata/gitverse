cdefdfc6044a6797b9f16fe8e3ef57b848da6b8f
Adarsh Kumar Debata <adarsh.debata@ad.iserveu.co.in>
2026-05-11 18:09:39 +0530

    fix(router): configure HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/gitverse">
+    {/* <BrowserRouter basename="/gitverse"> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
